61dd01dbdf11444bb07c2e9352e2c8f2fefd04f7
dekitsuneco <art199798@mail.ru>
2021-12-02 23:00:57 +0500

    finish the project

---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '7e02730684004f5797586c1468dc934d';
+const topHeadlinesUrl =
+    'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
